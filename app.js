cbeb2c190ce4d0194a4f1c467d68ae80c010ec7e
Daniel Chen <dchen8492@yahoo.com>
2018-04-27 12:19:21 -0400

    installing sql

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+// const index = require('./views');
+const layout = require('./views/layout.js')
+
+const PORT = 3000;
+
+app.use(morgan('dev'));
+app.use(express.static(`${__dirname}./public`));
+app.use('/', (req,res)=>{
+    res.send(layout(''))
+}) // line 4 already required;
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`listening on ${PORT}`)
+})
